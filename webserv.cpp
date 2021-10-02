14f56d2b87c15f633392708b5f8e2d2b1f342dd6
Jaime Serrano gonzalez <jserrano@c1r4s1.42madrid.com>
2021-10-02 11:45:20 +0200

    Default commit message

--- webserv.cpp
+++ webserv.cpp
@@ -11,20 +11,65 @@
 /* ************************************************************************** */
 
 #include <sys/socket.h>
+#include <netinet/in.h>
 #include <iostream>
 #include <stdlib.h>
 
-int main(int argc, char const **argv)
+#define PORT 8080
+
+class Server
 {
-	int socket_fd;
-	struct sockaddr_in serv_addr;
-	int addrlen = sizeof(serv_addr);
+	private:
+		int socket_fd, connect_fd;
+		struct sockaddr_in address;
+		const int addrlen;
+
+	public:
+		Server();
+		~Server();
+		
+		int getSocket();
+		int getConnection();
+		struct sockaddr_in getAddress();
+		int getAddrlen();
+};
+
+int main(void)
+{
+	int socket_fd, connection;
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
 
 	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		std::cerr << "Could not open socket" std::endl;
-		exit(EXIT_FAILURE);
+		std::cerr << "Could not open socket" << std::endl;
+		return (EXIT_FAILURE);
 	}
 
-	if ()
+	address.sin_family = AF_INET;
+	address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons( PORT );
+	for (int i = 0; i < sizeof(address.sin_zero); i++)
+		address.sin_zero[i] = 0;
+
+	if (bind(socket_fd, (struct sockaddr *)&address, addrlen) < 0)
+	{
+		std::cerr << "Could not bind socket" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	if (listen(socket_fd, 20) < 0)
+	{
+		std::cerr << "Could not create socket queue" << std::endl;
+		return (EXIT_FAILURE);
+	}
+	while (true)
+	{
+		if ((connection = accept(socket_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+		{
+			std::cerr << "Connection refused" << std::endl;
+			return (EXIT_FAILURE);
+		}
+
+
+	}
 }
