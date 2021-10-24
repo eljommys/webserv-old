d07fb82bf29a31bf708acc7108f0766bc8c8c8ed
jserrano <jserrano@student.42madrid.com>
2021-10-24 22:41:52 +0200

    Default commit message

--- srcs/parser/config.cpp
+++ srcs/parser/config.cpp
@@ -2,6 +2,8 @@
 
 std::string		get_first_block(const std::string &name, std::string text)
 {
+	if (text.find(name) == std::string::npos)
+		return "";
 	std::string::iterator i = text.begin() + text.find(name) + name.size();
 
 	i = text.begin() + text.find('{', i - text.begin()) + 1;
@@ -24,6 +26,8 @@ std::string		get_first_block(const std::string &name, std::string text)
 
 std::string		get_value(const std::string &name, std::string text)
 {
+	//std::cout << name << std::endl;
+
 	std::string::iterator begin = text.begin() + text.find(name) + name.size();
 	std::string::iterator end;
 	while (0 < *(begin.base()) && *(begin.base()) < 33)
@@ -43,17 +47,17 @@ std::vector<std::string>	get_vector(std::string str)
 	int							j = 0;
 	std::string::iterator		i = str.begin();
 
-	std::cout << "str = " << str << std::endl;
+	//std::cout << "str = " << str << std::endl;
 	while (i < str.end())
 	{
 		while (*(i.base()) == ' ')
 			i++;
-		std::cout << "next space is " << str.find(' ', i - str.begin()) << std::endl;
+		//std::cout << "next space is " << str.find(' ', i - str.begin()) << std::endl;
 		if (str.find(' ', i - str.begin()) != std::string::npos)
 			vector.push_back(str.substr(i - str.begin(), str.find(' ', i - str.begin()) - (int)(i - str.begin())));
 		else
 			vector.push_back(str.substr(i - str.begin(), str.end() - i));
-		std::cout << "vector[" << j << "] = ->" << vector[j] << "<-" << std::endl;
+		//std::cout << "vector[" << j << "] = ->" << vector[j] << "<-" << std::endl;
 		i += vector[j].size();
 		j++;
 	}
@@ -75,8 +79,11 @@ struct Config	parse_config(const std::string &str)
 	stream << file_stream.rdbuf();
 	std::string text(stream.str().c_str());
 
-	for (char j = 1; j < 31 && j != '\t'; j++)
-		text.erase(std::remove(text.begin(), text.end(), j), text.end());
+	for (char j = 1; j < 31; j++)
+	{
+		if (j != '\t')
+			text.erase(std::remove(text.begin(), text.end(), j), text.end());
+	}
 	text.erase(std::remove(text.begin(), text.end(), 127), text.end());
 	std::replace(text.begin(), text.end(), '\t', ' ');
 
@@ -88,9 +95,15 @@ struct Config	parse_config(const std::string &str)
 	config.index = get_vector(get_value("index", text));
 
 	int j = 0;
+	//std::cout << "text ============" << std::endl;
+	//std::cout << text << std::endl << std::endl;
 	std::string t_server = get_first_block("server", text);
+	int t_server_size;
 	while (t_server[0])
 	{
+		t_server_size = t_server.size();
+		//std::cout << "server #" << j << std::endl;
+		//std::cout << t_server << std::endl << std::endl;
 		struct V_server	server;
 		std::stringstream num(get_value("listen", t_server));
 		num >> server.port;
@@ -99,16 +112,28 @@ struct Config	parse_config(const std::string &str)
 		std::string t_location = get_first_block("location", t_server);
 		while (t_location[0])
 		{
+			//std::cout << "location before " << k << std::endl;
+			//std::cout << t_location << std::endl << std::endl;
 			struct Location location;
-			location.path = get_value("location", text);
-			location.autoindex = (get_value("autoindex", t_server) == "on") ? true : false;
-			location.root = get_value("root", t_server);
+			location.path = get_value("location", t_server);
+			location.methods = get_vector(get_value("methods", t_server));
+			location.autoindex = (get_value("autoindex", t_location) == "on") ? true : false;
+			location.root = get_value("root", t_location);
 			server.location.push_back(location);
-			t_server = (t_server.begin() + t_location.size()).base(); //PROBABLEMENTE ESTO DE PROBLEMAS (EL CONTENDIDO DEL BLOQUE NO ES LO QUE MIDE ENTERAMENTE)
+			t_server = (t_server.begin() + t_server.find("location") + t_location.size()).base();
+			t_location = get_first_block("location", t_server);
+			//std::cout << "t_location after================" << std::endl;
+			//std::cout << "->" << t_location << "<-" << std::endl << std::endl;
+			k++;
 		}
 		config.servers.push_back(server);
-		text = (text.begin() + t_server.size()).base();
+		std::cout << "text ===================" << std::endl;
+		std::cout << text << std::endl << std::endl;
+		text = (text.begin() + text.find("server") + t_server_size + std::string("server").size()).base();
 		t_server = get_first_block("server", text);
+		j++;
+		//if (j > 2)
+			//exit(1);
 	}
 
 	return config;
